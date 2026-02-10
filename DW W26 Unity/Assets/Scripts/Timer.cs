2852098ae01953e12d44cec5ed715cd9f90cfdfe
Connor-DewDrop-87 <000971900@mohawkcollege.ca>
2026-02-10 16:51:08 -0500

    Add Timer

--- DW W26 Unity/Assets/Scripts/Timer.cs
+++ DW W26 Unity/Assets/Scripts/Timer.cs
@@ -5,16 +5,19 @@ public class Timer : MonoBehaviour
 {
     public TMP_Text TimerUI;
     private float timeTotal;
+    private float timeDisplayed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timeTotal = 0;
+        timeDisplayed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         timeTotal += Time.deltaTime;
-        TimerUI.text = $"Time: {timeTotal}";
+        timeDisplayed = Mathf.RoundToInt(timeTotal);
+        TimerUI.text = $"Time: {timeDisplayed}";
     }
 }
