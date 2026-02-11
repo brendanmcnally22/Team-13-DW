bce28d5675be329361f45d35afae203da64a7eb3
Connor-DewDrop-87 <000971900@mohawkcollege.ca>
2026-02-11 12:15:54 -0500

    Update Timer
    Now displays minutes and seconds

--- DW W26 Unity/Assets/Scripts/Timer.cs
+++ DW W26 Unity/Assets/Scripts/Timer.cs
@@ -4,20 +4,32 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     public TMP_Text TimerUI;
-    private float timeTotal;
-    private float timeDisplayed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        timeTotal = 0;
-        timeDisplayed = 0;
-    }
-
+    private float seconds = 0;
+    private float minutes = 0;
+    private float secondsDisplayed = 0;
     // Update is called once per frame
     void Update()
     {
-        timeTotal += Time.deltaTime;
-        timeDisplayed = Mathf.RoundToInt(timeTotal);
-        TimerUI.text = $"Time: {timeDisplayed}";
+        seconds += Time.deltaTime;
+        // Display only seconds rather than microseconds or milliseconds
+        secondsDisplayed = Mathf.Floor(seconds);
+        // Reset seconds and increase minutes when seconds is 60 or more
+        if (secondsDisplayed >= 60)
+        {
+            secondsDisplayed = 0;
+            seconds = 0;
+            minutes += 1;
+        }
+        // If the seconds does not have a 10s place, then display it with a 0
+        if (secondsDisplayed < 10)
+        {
+            TimerUI.text = $"Time {minutes}:0{secondsDisplayed}";
+        }
+        // Otherwise, show it as normal
+        else
+        {
+            TimerUI.text = $"Time {minutes}:{secondsDisplayed}";
+        }
+            
     }
 }
