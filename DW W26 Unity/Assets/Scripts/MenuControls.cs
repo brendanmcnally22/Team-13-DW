574fdc925d0ce175492d0588376dcd12df55cb27
Connor-DewDrop-87 <000971900@mohawkcollege.ca>
2026-02-12 16:36:31 -0500

    Add Story Screen Base

--- DW W26 Unity/Assets/Scripts/MenuControls.cs
+++ DW W26 Unity/Assets/Scripts/MenuControls.cs
@@ -5,6 +5,7 @@ public class MenuControls : MonoBehaviour
 {
     public float selection;
     bool ControlTutorialUp = false;
+    bool StoryUp = false;
     bool buttonDown;
     [Space(10)]
     [Header("Start")]
@@ -22,6 +23,9 @@ public class MenuControls : MonoBehaviour
     [Header("Controls Screen")]
     public GameObject ControlTutorial1;
     public GameObject ControlsMainMenu;
+    [Space(10)]
+    [Header("Story Screen")]
+    public GameObject StoryMenu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -33,10 +37,10 @@ public class MenuControls : MonoBehaviour
     void Update()
     {
         // Don't show controller tutorial and allow player to move
-        if (ControlTutorialUp == false)
+        if (ControlTutorialUp == false && StoryUp == false)
         {
             ControlTutorial1.SetActive(false);
-            
+            StoryMenu.SetActive(false);
             ControlsMainMenu.SetActive(true);
             float verticalInput = Input.GetAxis("Vertical");
             if (verticalInput == 0)
@@ -73,10 +77,14 @@ public class MenuControls : MonoBehaviour
             }
         }
         // Show Controller Tutorial and don't allow main menu movement
-        else
+        if (ControlTutorialUp == true)
         {
             ControlTutorial1.SetActive(true);
-         
+            ControlsMainMenu.SetActive(false);
+        }
+        if (StoryUp == true)
+        {
+            StoryMenu.SetActive(true);
             ControlsMainMenu.SetActive(false);
         }
         // Start is Selected
@@ -118,6 +126,10 @@ public class MenuControls : MonoBehaviour
             ControlsSelected.SetActive(false);
             QuitSprite.SetActive(false);
             QuitSelected.SetActive(true);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                StoryUp = !StoryUp;
+            }
         }
     }
 }
